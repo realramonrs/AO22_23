fcf477bbb320c9400a9e5f5faaafa4ef1b20b228
RamonRS <celmon1@gmail.com>
2022-09-12 13:03:26 +0200

    Día 1

---
+++ Tema 1/Ejemplo01_12Septiembre/Ejemplo01_12Septiembre/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        'Esto es un comentario
+        'Saludar al usuario : 
+        Console.Write("Hola Mundo. Me encanta programar con Ramón")
+        'Mantener la consola abierta hasta que el usuario pulsa
+        'la tecla Enter
+        'Cambiar el título de la consola
+
+        Console.ReadLine()
+        Console.Title = "Mi primer programa"
+    End Sub
+
+End Module
