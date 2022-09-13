1ff60f52dd5147f72d05d1487ce89e883dd3c0bc
RamonRS <celmon1@gmail.com>
2022-09-13 13:01:03 +0200

    Dia3

--- Tema 1/BoletinTema1/BoletinTema1/Module1.vb
+++ Tema 1/BoletinTema1/BoletinTema1/Module1.vb
@@ -1,6 +1,15 @@
 ﻿Module Module1
 
     Sub Main()
+        'Ejercicio 8
+
+
+        'Ejercicio 9
+
+        Console.WriteLine(" ****")
+        Console.WriteLine("*")
+        Console.WriteLine("***")
+        Console.ReadLine()
 
     End Sub
 
---
+++ Tema 2/Ejemplo01_13Septiembre/Ejemplo01_13Septiembre/Module1.vb
@@ -0,0 +1,32 @@
+﻿
+Module Module1
+
+    Sub Main()
+        '****** Zona declaración de variables *******
+        'Reservamos memoria para nuestro programa
+        Dim b1 As Byte
+        Dim i1 As Integer = 150  'Declaración e inicialización
+        Dim l1 As Long
+        l1 = 450000000
+
+        Dim s1 As Single = 4.56
+        Dim d1 As Double
+
+        '********************************************'
+        '***** Visualizamos los valores de las variables
+        Console.WriteLine("b1 = " & b1)
+        'Visualizar máximo valor que almacena el tipo Byte
+        Console.WriteLine("Máximo valor de byte : " & Byte.MaxValue)
+        Console.WriteLine("Minimo valor de byte : " & Byte.MinValue)
+
+        Console.WriteLine("s1 = " & s1)
+        'Modificar valores de variables
+        b1 = 90
+
+        i1 = i1 + 10
+        s1 = 5.8888859
+        Console.WriteLine("s1 = " & s1)
+        Console.ReadLine()
+    End Sub
+
+End Module
