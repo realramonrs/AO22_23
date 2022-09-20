579ce233ec854a3a090ddfb3cced3a971bc7254b
RamonRS <celmon1@gmail.com>
2022-09-20 11:51:07 +0200

    Dia 20

---
+++ Tema 2/Ejemplo05_ConversionesSegundaParte_20Septbre/Ejemplo05_ConversionesSegundaParte_20Septbre/Module1.vb
@@ -0,0 +1,28 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        'Con option strict off , visual realiza conversiones de tipo sin avisar
+        Dim numero As Integer = Convert.ToInt32(4.567)
+        Console.WriteLine("Numero: " & numero)
+
+        Dim x As Byte
+        Dim y As Integer = 456
+
+
+        x = Convert.ToByte(y)
+
+        Console.WriteLine("X = " & x) 'OverflowException
+
+        Dim n1 As Integer = 51
+        Dim resultado As Integer = Convert.ToInt32(n1 / 7)
+
+        Dim v As Byte = 245
+        v = Convert.ToByte(v + 100) 'OverflowException
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
