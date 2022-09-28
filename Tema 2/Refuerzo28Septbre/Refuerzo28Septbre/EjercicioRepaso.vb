25f441768c2daf301bb9b08cc913a0388f8dd89f
RamonRS <celmon1@gmail.com>
2022-09-28 16:48:50 +0200

    Refuerzo Día 28

---
+++ Tema 2/Refuerzo28Septbre/Refuerzo28Septbre/EjercicioRepaso.vb
@@ -0,0 +1,3 @@
+﻿Module EjercicioRepaso
+
+End Module
---
+++ Tema 2/Refuerzo28Septbre/Refuerzo28Septbre/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        'Siempre es posible asignar el valor de una variable de menor tamaño a
+        'una variable de mayor tamaño
+        'byte(8 bits)--> short(16bits) --> Integer(32bits) -->Long(64bits)
+        Dim b As Byte = 200
+        Dim s As Short = b
+
+        'Las asignaciones de variables de mayor tamaño a variables de menor tamaño,
+        'no son siempre posibles, y por defecto no nos dejan hacerlo directamente
+        Dim s2 As Short = 400
+        Dim b2 As Byte = Convert.ToByte(s2)
+
+
+
+
+    End Sub
+
+End Module
