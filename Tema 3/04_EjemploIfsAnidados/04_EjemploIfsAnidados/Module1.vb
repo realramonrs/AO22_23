5292a2cebd1f5a5218783baff028d0e578e88dd5
RamonRS <celmon1@gmail.com>
2022-10-11 10:05:48 +0200

    11 Octubre

---
+++ Tema 3/04_EjemploIfsAnidados/04_EjemploIfsAnidados/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        'Programa que me diga si un número es > 100 y/o Múltiplo de 6
+
+        Dim numero As Integer
+        Dim continuar As Char
+        Do
+            Console.WriteLine("Introduce un número.")
+            numero = Convert.ToInt32(Console.ReadLine())
+
+            If numero Mod 6 = 0 Then
+                If numero > 100 Then
+                    Console.WriteLine("Múltiplo de 6 y > 100")
+                Else
+                    Console.WriteLine("Múltiplo de 6 y <= 100")
+
+                End If
+            Else
+                If numero > 100 Then
+                    Console.WriteLine("Numero > 100 y no es múltiplo de 6 ")
+                Else
+                    Console.WriteLine("Numero <= 100 y no es múltiplo de 6")
+                End If
+            End If
+
+
+            Console.WriteLine("Desea continuar s/n")
+            continuar = Convert.ToChar(Console.ReadLine())
+
+        Loop While continuar = "s"c
+    End Sub
+
+End Module
---
+++ Tema 3/05_SelectCase/05_SelectCase/Module1.vb
@@ -0,0 +1,67 @@
+﻿
+Option Strict On
+Module Module1
+
+    Sub Main()
+        'Select Case --> Evaluar valores discretos de una variables
+        Dim opcion As Char
+        Dim contador As Byte
+        Do
+            'Ejemplo menú:
+            Console.WriteLine("Escoja una opción: ")
+            Console.WriteLine("a. Calcular la raíz de un número")
+            Console.WriteLine("b. Obtener el menor de tres números")
+            Console.WriteLine("c. Decir si un número es múltiplo de 7")
+            Console.WriteLine("d. Salir")
+            opcion = Convert.ToChar(Console.ReadLine())
+
+            'Evaluar la variable opción
+
+            'If opcion = "a"c Then
+
+            'ElseIf opcion = "b"c Then
+            'ElseIf opcion = "c"c Then
+            'Else
+
+            'End If
+
+            Select Case opcion
+                Case "a"c, "A"c
+                    Console.WriteLine("Opción a: ")
+
+                Case "b"c, "B"c
+                    Console.WriteLine("Opción b")
+                Case "c"c, "C"c
+                    Console.WriteLine("Opción c")
+                    Dim numero As Integer
+                    Console.WriteLine("Intro número: ")
+                    numero = Convert.ToInt32(Console.ReadLine())
+
+                    If numero Mod 7 = 0 Then
+                        Console.WriteLine("Múltiplo de 7")
+                    End If
+
+                Case "d"c
+                    Console.WriteLine("Adios. Pulse Enter para confirmar.")
+
+                Case Else
+                    Select Case contador
+                        Case 0
+                            Console.WriteLine("Error en la opción, te quedan 2 intentos.")
+
+                        Case 1
+                            Console.WriteLine("Error en la opción, te queda 1 intentos.")
+                        Case 2
+                            Console.WriteLine("Error en la opción, Se acabó.")
+                    End Select
+                    contador += Convert.ToByte(1)
+            End Select
+
+
+
+                    Console.ReadLine()
+        Loop While opcion <> "d"c And contador < 3
+
+    End Sub
+
+End Module
