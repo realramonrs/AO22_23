3fc1295a2b6b1728f2f85560ad44586ddacbdc25
RamonRS <celmon1@gmail.com>
2022-10-19 16:49:31 +0200

    ds

---
+++ Tema 3/Repaso19OctubreBis/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        'Programa que indica si un número es múltiplo de 2 , si es múltiplo de 3, o si es múltiplo de 7
+        'En caso de que no sea múltiplo de ninguno indica con un mensaje dicha situación
+        Dim numero As Integer = 20
+
+        If numero Mod 2 = 0 Then
+            Console.WriteLine("múltiplo de 2")
+
+        End If
+    End Sub
+
+End Module
